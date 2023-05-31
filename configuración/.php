ab2bfb438cf18ffea4e1265aa46534548dc99959
mmpalenzuela <rpm00@iesemilidarder.com>
2023-05-31 10:07:03 +0200

    yhh

---
+++ configuración/.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+
+    private $hostname = "localhost";
+    private $database = "intercambio";
+    private $username = "raul";
+    private $password = "";
+    private $charset = "utf8";
+
+    function conectar(){
+        
+        $conexion = "mysql:host=" . $this->hostname . "; dbname="  . $this->hostname ."; charset"
+        
+    
+    
+    }
+   
+}
+
+?>
