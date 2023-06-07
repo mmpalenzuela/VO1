77182a1014fd0b46c64e2276a2a94dd543d95bc4
mmpalenzuela <rpm00@iesemilidarder.com>
2023-06-07 09:51:19 +0200

    justin

--- conf/database.php
+++ conf/database.php
@@ -1,35 +1,20 @@
 <?php
 
-class Database {
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "intercambio";
 
-    private $hostname = "localhost";
-    private $database = "intercambio";
-    private $username = "raul";
-    private $password = "";
-    private $charset = "utf8";
+$conexion = new mysqli($server, $user, $pass, $db);
 
-    function conectar(){
-        
-        try{
-        $conexion = "mysql:host=" . $this->$hostname . "; dbname="  . $this->$database . "; 
-        charset=".  $this->charset;
-        options = [
-            PDO::ATTR:_ERRMODE => PDO::ERROR_EXCEPTION,
-            PDO::ATTR:_EMULATE_PREPARES => PDO:: false
-       
-        ];
-
-        $pdo = new PDO($conexion, $this->username, $this->password, $this->$options);
-
-        return $pdo;
-        catch(PDOException $e){
-            echo 'Error conexion: ' . &e->getMessage();
-            exit;
-        }
-    
-    
-    }
-    }
+if ($conexion->connect_erno) {
+    die("conexion fallida" . $conexion->connect_arno);
 }
+ else {
+    echo "conectado";
+
+}
+
+
 
 ?>
